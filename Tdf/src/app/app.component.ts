f444c99375c7131d335dc7ae3e723bf744d105b8
sagor31h2 <sagor31h2@gmail.com>
2022-10-01 01:57:56 +0600

    two way binding

--- Tdf/src/app/app.component.ts
+++ Tdf/src/app/app.component.ts
@@ -1,10 +1,21 @@
+import { User } from './models/user';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Tdf';
+  Courses = ['Angular', 'React', 'vue'];
+
+  userModel = new User(
+    'Sagor',
+    'sagor@gmail.com',
+    '01828367733',
+    'angular',
+    'morning',
+    true
+  );
 }
