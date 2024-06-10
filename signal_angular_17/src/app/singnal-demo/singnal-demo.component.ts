8635f6d131bb5f724020569f688942c467778d81
sagor31h2 <sagor31h2@gmail.com>
2024-06-10 11:58:06 +0600

    project created

---
+++ signal_angular_17/src/app/singnal-demo/singnal-demo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singnal-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './singnal-demo.component.html',
+  styleUrl: './singnal-demo.component.scss'
+})
+export class SingnalDemoComponent {
+
+}
